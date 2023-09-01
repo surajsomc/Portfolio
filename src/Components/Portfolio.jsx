5011e59c2c8bc20cc628bd04061a53029089169f
Suraj Somarajan <46799958+surajsomc@users.noreply.github.com>
2023-09-01 07:45:29 +0000

    Pushed changes to github

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Optimized-Illustration-from-Adobe-Stock-for-ITC-Post-on-AI-in-Game-Development-scaled.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "MERN Full Stack Application: Property Management App",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "•	Developed a comprehensive property management web application using the MERN stack (MongoDB, Express.js, React.js, Node.js). Implemented a user-friendly interface for property managers and tenants to handle property-related tasks efficiently. Leveraged MongoDB for data storage and retrieval, Express.js for building a robust backend API, and React.js for building responsive and interactive front-end components. Utilized Node.js to manage server-side operations and handle authentication. Incorporated features like property listing, tenant management, rent payments, maintenance requests, and real-time communication using WebSocket. Implemented role-based access control to ensure data privacy and security. This project allowed me to showcase my full stack development skills while addressing real-world property management challenges effectively.",
+    url: "",
   },
   {
-    title: "Web Development for Beginners",
+    title: "MLH Mad Hacks Project: Round-Up Savings Mobile App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "•	During a hackathon, I took charge of developing a full stack mobile app utilizing my skills in React Native for front-end development, Node.js for backend implementation, and PostgreSQL for database management. The app aimed to revolutionize personal finance by automating savings and investments. My role involved designing a user-friendly interface for seamless experiences on both iOS and Android platforms, while leveraging Node.js to handle transaction processing, investment calculations, and real-time investment tracking. Additionally, I utilized PostgreSQL to efficiently store and retrieve user financial data, ensuring data security and privacy. The successful implementation of round-up functionality allowed users to automate savings and direct spare change into diversified investment portfolios, leading our team to secure an honorable mention in the hackathon.",
+    url: "",
   },
   {
-    title: "My Resume Site",
+    title: "AI Project: Emotion Recognition from Facial Expressions",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "•	Developed a deep learning model using Python, TensorFlow, and OpenCV to recognize emotions from facial expressions in images and video frames. Curated a diverse dataset, implemented a Convolutional Neural Network (CNN) with transfer learning, and achieved high accuracy. Conducted error analysis, fine-tuned the model, and created a user-friendly GUI for accessing the emotion recognition system. This project enhanced my AI and computer vision skills while contributing to applications in human-computer interactions and virtual reality experiences.",
+    url: "",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Software Engineering Intern: Vamaze Tech",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "•	Collaborated on machine learning projects utilizing Python, TensorFlow, and scikit-learn for data preprocessing and model development. Employed AWS cloud services such as EC2 and S3 to store and process large datasets, enhancing scalability and flexibility. Contributed to refining machine learning algorithms, resulting in improved model accuracy and documented project progress for effective team collaboration. Implemented version control using Git to track changes in the codebase and facilitate seamless collaboration among team members. Utilized advanced techniques such as hyperparameter tuning and cross-validation to optimize model performance, leading to enhanced predictive capabilities.",
+    url: "",
   },
 ];
 
